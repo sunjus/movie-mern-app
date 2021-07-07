b2f3aea71bef8023bf2820a4d4fad40d9417a7c0
sunjus <sunju.shin1108@gmail.com>
2021-07-07 20:11:56 +0300

    Changed navbar menu

--- client/src/components/views/LandingPage/Sections/ImageHeader.js
+++ client/src/components/views/LandingPage/Sections/ImageHeader.js
@@ -5,7 +5,7 @@ export const ImageHeader = ({ image, title, text }) => {
     <div
       style={{
         background: `linear-gradient(to bottom, rgba(0,0,0,0) 39%, rgba(0,0,0,0) 41%, rgba(0,0,0,0.65) 100%), url('${image}'), #1c1c1c`,
-        height: "500px",
+        height: "570px",
         width: "100%",
         position: "relative",
         backgroundSize: "100%, cover",
---
+++ client/src/components/views/NavBar/Sections/LeftMenu.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Menu } from "antd";
+import Logo from "../../../../assets/images/Logo1.png";
+
+function LeftMenu(props) {
+  return (
+    <Menu mode={props.mode}>
+      {/*<div className="menu__logo">
+      <Menu.Item key="mail">
+        <a href="/">
+          <img src={Logo} alt="Logo" />
+        </a>
+      </Menu.Item>*/}
+      <Menu.Item key="mail">
+        <a href="/">Home</a>
+      </Menu.Item>
+      <Menu.Item key="favorite">
+        <a href="/favorite">Favorite</a>
+      </Menu.Item>
+    </Menu>
+  );
+}
+
+export default LeftMenu;
